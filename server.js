be090a658f8a39e33740d4c6c77386896357872e
Bhuvan A J <ajbhuvan@gmail.com>
2023-08-23 23:28:48 +0530

    hmm

--- server.js
+++ server.js
@@ -6,22 +6,36 @@ const PORT = process.env.PORT || 8080;
 const baseUrl = '/calculator'
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('Hello World!');
+    // return res.send('');
+    res.status(200).send("Hello world!");
 });
 
+// baseRouter.get('/add', (req, res) => {
+//     res.sendFile(__dirname+"/index.html");
+// })
+
 baseRouter.post('/add', (req, res) => {
-    const { first, second } = req.body;
-    res.json({ "result": first + second });
+    // res.json({ "": null });
+    // res.setHeader('Content-Type', 'application/json')
+    // console.log(req.body)
+    var first=Number(req.body.first);
+    var second=Number(req.body.second);
+    var result=first+second;
+    res.status(200).json({ result: result });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    const { first, second } = req.body;
-    res.json({ "result": first - second });
+    // res.json({ "": null });
+    var first=Number(req.body.first);
+    var second=Number(req.body.second);
+    var result=first-second;
+    res.status(200).json({ result: result });
 });
 
 app.use(baseUrl, baseRouter);
